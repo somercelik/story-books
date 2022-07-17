b8ec43b33f7320aa1aaaf495208664e1ac30adb4
somercelik <somercelik@yahoo.com>
2022-07-17 18:49:56 +0300

    Added layouts, routes, css libs and db con

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+const connectDB = require("./config/db");
 
 // Load config
 dotenv.config({
     path: "config/config.env"
 });
 
+connectDB();
+
 const app = express();
+
+
+// Logging
+if(process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
+
+// Handlebars
+app.engine(".hbs", engine({extname: ".hbs"}));
+app.set("view engine", ".hbs");
+
+// Static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.use("/", require("./routes/index"));
 
 const PORT = process.env.BACKEND_PORT;
 
