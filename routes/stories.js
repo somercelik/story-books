77b3f1cad3111829cdfee05cf8db8c21386b9ad3
somercelik <somercelik@yahoo.com>
2022-07-18 06:28:34 +0300

    Minor reformatting

--- routes/stories.js
+++ routes/stories.js
@@ -73,7 +73,7 @@ router.get("/:id", ensureAuth, async (req, res) => {
             .populate("user")
             .lean();
 
-        if(!story) {
+        if (!story) {
             return res.render("error/404");
         }
 
@@ -153,6 +153,28 @@ router.delete("/:id", ensureAuth, async (req, res) => {
             })
             res.redirect("/dashboard");
         }
+    } catch (err) {
+        console.error(err);
+        res.render("error/500");
+    }
+})
+
+/**
+ * @desc    Render User stories page
+ * @route   GET /stories/user/:userId
+ */
+router.get("/user/:userId", ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({
+                user: req.params.userId,
+                status: "public"
+            })
+            .populate("user")
+            .lean();
+
+        res.render("stories/index", {
+            stories
+        })
     } catch (err) {
         console.error(err);
         res.render("error/500");
